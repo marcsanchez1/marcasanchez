95c6a61cefd1d9d009d6d532569777dd702a5aa4
Marc Sanchez <marcsanchez@me.com>
2020-05-23 16:21:58 -0400

    Updates

--- js/script2.js
+++ js/script2.js
@@ -1,6 +1,6 @@
 // Sticky navigation
 $(".main-nav").sticky({
-  topSpacing:0
+  topSpacing: 0
 });
 
 $('.portfolio-hero-btn').hide();
@@ -8,10 +8,8 @@ $('.portfolio-hero-btn').hide();
 
 $('.hero-header').hide().slideDown(2000);
 $('.hero-paragraph').hide().fadeIn(3000);
-// $('.portfolio-hero-btn').hide().delay(1000).fadeIn(3000);
-// $('.contact-hero-btn').hide().delay(1000).fadeIn(3000);
 
 setTimeout(function() {
-  $('.portfolio-hero-btn').fadeIn(4000);
-  $('.contact-hero-btn').fadeIn(4000);
+  $('.portfolio-hero-btn').fadeIn();
+  $('.contact-hero-btn').fadeIn();
 }, 2000);
