8ee16c52a9925f0ffef52a407819a0417aefad50
Marc Sanchez <marcsanchez@me.com>
2020-06-02 00:37:11 -0400

    added tech-icon carousel

--- js/script.js
+++ js/script.js
@@ -14,3 +14,43 @@ setTimeout(function() {
   $('.portfolio-hero-btn').slideDown();
   $('.contact-hero-btn').slideDown();
 }, 2000);
+
+// Slides in the about me section
+
+    $('.slides').slick({
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 5,
+      slidesToScroll: 5,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
